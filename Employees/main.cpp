6a7576256812d2bf2df00c872a09c11d325314d1
Assenti <asset.sultan@gmail.com>
2018-04-24 17:08:31 +0600

    Employees homework: DONE

--- Employees/main.cpp
+++ Employees/main.cpp
@@ -6,12 +6,12 @@ int main()
 	Google.addEmployee("John", "Mathew", "Middle Developer", 4, 80000.50);
 	Google.addEmployee("Brad", "Davis", "Senior Developer", 8, 120000.70);
 	Google.addEmployee("Ken", "Alan", "Senior Developer", 9, 125000.50);
-	Google.addEmployee("John", "L", "Junior Developer", 1, 50000.50);
-	Google.addEmployee("John", "Mathew", "Middle Developer", 4, 80000.50);
-	Google.addEmployee("John", "Mathew", "Middle Developer", 4, 80000.50);
-	Google.addEmployee("John", "Mathew", "Middle Developer", 4, 80000.50);
+	Google.addEmployee("Ben", "Larry", "Junior Developer", 1, 50000.50);
+	Google.addEmployee("Abraham", "Queen", "Senior Developer", 7, 120000.50);
+	Google.addEmployee("Isaak", "Zuckerberg", "Middle Developer", 5, 95000.80);
+	Google.addEmployee("Arman", "Suleimenov", "Senior Developer", 10, 130000.70);
 
-	Google.filterByPosition("Senior Developer")
+	Google.showFilterResults(Google.filterByPosition("Senior Developer"));
 
 	system("pause");
 	return 0;
