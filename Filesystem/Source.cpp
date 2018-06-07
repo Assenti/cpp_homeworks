3058eea27407708ae438ee0d17a020d410c7e65d
СултановА <@224_4>
2018-06-07 21:58:58 +0600

    filemanager

---
+++ Filesystem/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+//#include <experimental\filesystem>
+
+
+int main()
+{
+	/*std::experimental::filesystem::path path;
+	std::cout << path.filename.string << std::endl;*/
+	
+	//Create file
+	std::string dir_name;
+	std::cin >> dir_name;
+
+
+
+	//Delete file
+
+	system("pause");
+	return 0;
+}
